cf9df9d1d3d2653a961fda9bbdbf32e2236cddb3
Paul Hedman <paul@mybb.com>
2014-02-08 18:41:30 -0600

    Create deleteaccount.lang.php

---
+++ inc/languages/english/deleteaccount.lang.php
@@ -0,0 +1,30 @@
+<?php
+//English language pack for Delete Account plugin by Paul H./MyBB Security
+
+$l['da_are_you_sure'] = 'Are you sure you want to delete your account?  If so, continue.  If not, hit the back button on your browser or close the page.';
+$l['da_confirm_account_deletion'] = 'Are you sure you want to PERMANENTLY delete this account?  All information, including private messages, custom profile fields, etc, will be PERMANENTLY deleted.';
+$l['da_delete'] = 'Permanently Delete User';
+$l['da_deleted'] = 'Permanently deleted';
+$l['da_delete_account'] = 'Delete Account';
+$l['da_deleted_accounts'] = 'Deleted Accounts';
+$l['da_deleted_accounts_desc'] = 'Here you can view/restore all accounts deleted by the Delete Account plugin.';
+$l['da_delete_account_name'] = 'Delete Account';
+$l['da_delete_account_desc'] = 'Allow users to delete their account.  Account can be restored (or permanently deleted) by admins.';
+$l['da_delete_reason'] = 'Reason for account deletion';
+$l['da_delete_time'] = 'Account deleted on:';
+$l['da_invalid_user'] = 'Invalid User';
+$l['da_no_deleted_accounts'] = 'No deleted accounts.';
+$l['da_permanently_deleted'] = 'The user has been permanently deleted.';
+$l['da_user_restored'] = 'User has been restored successfully!';
+$l['da_redirect'] = 'Your account has been deleted.  If you change your mind an administrator may be able to restore it for you.';
+$l['da_restore'] = 'Restore User';
+$l['da_restored'] = 'Restored user';
+$l['da_why'] = 'Please give us some information on why you are deleting your account.';
+$l['da_yes'] = 'Yes, I want to delete my account.';
+
+$l['da_settings_name'] = 'Delete Account Settings';
+$l['da_settings_desc'] = 'Settings for the Delete Account plugin.';
+
+$l['da_settings_normgroup_name'] = 'Groups';
+$l['da_settings_normgroup_desc'] = 'A CSV of groups that can delete their accounts.';
+?>
